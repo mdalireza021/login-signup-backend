60adbb3e3718696f7a901b168f9976a2338032ed
md ali reza <muhammd.ali@brainstation-23.com>
2023-06-26 11:48:04 +0600

    fix token issue

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,62 +1,62 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import User from '../models/user.model.js'
-import { secret } from '../config/auth.config.js';
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import User from "../models/user.model.js";
+import { secret } from "../config/auth.config.js";
 const saltRounds = 10;
 
 export const login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-        const user = await User.findOne({ email }).exec();
-        if (!user) {
-            return res.status(404).send({ message: 'User Not found.' });
-        }
-
-        const passwordIsValid = bcrypt.compareSync(password, user.password);
-
-        if (!passwordIsValid) {
-            return res.status(401).send({ message: 'Invalid Password!' });
-        }
-
-        const token = jwt.sign({ id: user.id }, secret, {
-            expiresIn: '72h',
-        });
-
-        req.session.token = token;
-
-        res.status(200).send({
-            id: user._id,
-            username: user.username,
-            email: user.email,
-        });
-    } catch (err) {
-        res.status(500).send({ message: err.message });
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      return res.status(404).send({ message: "User Not found." });
     }
+
+    const passwordIsValid = bcrypt.compareSync(password, user.password);
+
+    if (!passwordIsValid) {
+      return res.status(401).send({ message: "Invalid Password!" });
+    }
+
+    const token = jwt.sign({ id: user.id }, secret, {
+      expiresIn: "72h",
+    });
+
+    res.cookie("token", token, { httpOnly: true });
+
+    res.status(200).send({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 export const signup = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const hash = bcrypt.hashSync(password, saltRounds);
-        const user = new User({
-            username,
-            email,
-            password: hash,
-        });
+  try {
+    const { username, email, password } = req.body;
+    const hash = bcrypt.hashSync(password, saltRounds);
+    const user = new User({
+      username,
+      email,
+      password: hash,
+    });
 
-        const savedUser = await user.save();
-        return res.status(201).json({ message: 'Signup successfully!' });
-    } catch (err) {
-        console.error(err);
-        return res.status(500).json({ message: 'Internal server error' });
-    }
+    const savedUser = await user.save();
+    return res.status(201).json({ message: "Signup successfully!" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const signout = async (req, res) => {
-    try {
-        req.session = null;
-        return res.status(200).send({ message: "You've been signed out!" });
-    } catch (err) {
-        this.next(err);
-    }
+  try {
+    res.clearCookie("token");
+    return res.status(200).send({ message: "You've been signed out!" });
+  } catch (err) {
+    this.next(err);
+  }
 };
