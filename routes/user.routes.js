e096a8823516befb215d3eb335ac5ca09e64db34
md ali reza <muhammd.ali@brainstation-23.com>
2023-06-26 20:52:10 +0600

    update user route

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,22 +1,19 @@
-import express from 'express';
-import {verifyToken} from '../middlewares/verifyToken.js';
+import express from "express";
+import { verifyToken } from "../middlewares/verifyToken.js";
 import { allAccess, userBoard } from "../controllers/user.controller.js";
 const userRouter = express.Router();
 
-userRouter.use(() => {
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, Content-Type, Accept"
-    );
-    next();
-})
+userRouter.use((req, res, next) => {
+  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+  next();
+});
 
 // simple route
 userRouter.get("/", (req, res) => {
-    res.json({ message: "Welcome to new world!" });
+  res.json({ message: "Welcome to new world!" });
 });
 
 userRouter.get("/all", allAccess);
-userRouter.get("/user", verifyToken, userBoard);
+userRouter.get("/user", [verifyToken], userBoard);
 
 export default userRouter;
